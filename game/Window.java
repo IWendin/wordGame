3bfa1f32ac1cbc7fedf961c145b6bcc9bda4c5ea
Ingrid <ingwe018@student.liu.se>
2019-07-17 20:19:31 +0200

    Window class added

--- game/Window.java
+++ game/Window.java
@@ -4,26 +4,37 @@ import java.awt.event.*;
 
 public class Window {
 	JFrame window;
-//	Container con;
 	JTextField jtf;
-//	JTextArea jta;
+	JTextArea jta;
+	JPanel jp;
+	JScrollPane scroll;
+	private final static String nl = "\n";
 	
 	public Window() {
 		//add(jtf);//, BorderLayout.SOUTH);
 		window = new JFrame("Magic returns");
 		window.setSize(800,500);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+		
 		
 	//	con = window.getContentPane();
 		
 		jtf = new JTextField();
 		jtf.setBounds(0, 400, 800, 30);
 		jtf.addActionListener(action);
-	//	jta = new JTextArea(30,50);
+		jta = new JTextArea(30,50);
+		jta.setEditable(false);
+		jta.setBounds(0,10,780,350);
+		scroll = new JScrollPane(jta);
+		scroll.setBounds(0, 10, 780, 350);
 		
-	//	con.add(jtf);
-		window.add(jtf);
+		jp = new JPanel();
+		//jp.add(jtf); // seems like jtf does not work with the JPane
+		jp.add(scroll);
+		
+		window.add(jtf);	//move to JFrame instead of JPane
+	//	window.add(jta);
+		window.add(scroll);
 		window.setLayout(null);
 		window.setVisible(true);
 	}
@@ -31,7 +42,10 @@ public class Window {
 	Action action = new AbstractAction() {
 		@Override
 		public void actionPerformed(ActionEvent enterPressed) {
-			System.out.println("Enter has been pressed");
+			String text = jtf.getText();
+			jtf.setText("");
+			jta.append(text + nl);
+			
 		}
 	};
 	
